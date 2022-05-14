3c1e79fedf9d03e47d307449305f313bb30dbbbe
marait123 <max.ibrahim43@hotmail.com>
2022-05-14 19:14:52 +0200

    now I can read the lidar laser scan and also change the script runner to python2

--- scripts/receiver.py
+++ scripts/receiver.py
@@ -1,13 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 import rospy
 from std_msgs.msg import String, Float64
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import LaserScan
 
 def subscriberCallBack(data):
-    rospy.loginfo(rospy.get_caller_id() + " I recieved the following --  %s", data.data) #prints on terminal
+    rospy.loginfo("\n============================================================\n")
+    rospy.loginfo(rospy.get_caller_id() + " I recieved the following --  %s", data) #prints on terminal
+    rospy.loginfo("\n============================================================\n")
     
 def listener():
     rospy.init_node('subscriberNode', anonymous=True)
-    rospy.Subscriber("talker", Float64, subscriberCallBack)
+    rospy.Subscriber("scan", LaserScan, subscriberCallBack)
     rospy.spin() # the python file does not exit
 
 if __name__ == '__main__':
